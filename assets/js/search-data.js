f186bceb820e6e37d3614bebf66ba36ad4c4ca77
hiacelab <hiacelab.imse@ttu.edu>
2026-04-15 00:47:39 +0000

    Deploying to gh-pages from @ hiacelab/hiacelab.github.io@003e374803224e8b7c03f3e27ce7b91a74b6998e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,6 +451,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-icr-drive",
+          title: 'ICR-Drive',
+          description: "Instruction Counterfactual Robustness for End-to-End Language-Driven Autonomous Driving",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/icrdrive/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
