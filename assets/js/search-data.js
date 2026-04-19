ffecb4e3a578caa812766830f991978ba035e68b
hiacelab <hiacelab.imse@ttu.edu>
2026-04-19 06:47:13 +0000

    Deploying to gh-pages from @ hiacelab/hiacelab.github.io@c11a7e09de750a110e99f9c7201305525147e650 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -425,8 +425,8 @@ ninja.data = [{
           description: "Peihang received the HFES PPTG Student Research Grant",
           section: "News",handler: () => {
               window.location.href = "/news/2025-10-12-peihang-pptg-grant/";
-            },},{id: "news-acceptance-peihang-li-to-present-at-the-69th-hfes-international-annual-meeting",
-          title: '[Acceptance] Peihang Li to Present at the 69th HFES International Annual Meeting',
+            },},{id: "news-publication-peihang-li-to-present-at-the-69th-hfes-international-annual-meeting",
+          title: '[Publication] Peihang Li to Present at the 69th HFES International Annual Meeting',
           description: "Upcoming HFES 2025 presentation by Peihang Li",
           section: "News",handler: () => {
               window.location.href = "/news/2025-10-12-peihang-to-present-at-hfes-2025/";
