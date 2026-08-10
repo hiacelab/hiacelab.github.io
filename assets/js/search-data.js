fa1d38597ce2df78e057d697b2cc4c74cce4d840
hiacelab <hiacelab.imse@ttu.edu>
2026-08-10 18:33:20 +0000

    Deploying to gh-pages from @ hiacelab/hiacelab.github.io@27d9f41cc4c2450e9f8d49e00a64cfdf05c00c73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-people",
-          title: "people",
-          description: "members of the HiACE Lab",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "nav-publications",
+  },{id: "nav-publications",
           title: "publications",
           description: "selected publications representing recent and foundational work from the HiACE Lab",
           section: "Navigation",
